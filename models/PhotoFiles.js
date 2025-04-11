10cfe9f26b05f5e05ac2d0a6214596f89228d630
Kartik Dangi <114815601+MrkartikDangi@users.noreply.github.com>
2025-04-11 19:31:19 +0530

    Update PhotoFiles.js

--- models/PhotoFiles.js
+++ models/PhotoFiles.js
@@ -6,6 +6,7 @@ const PhotoFileSchema = new mongoose.Schema(
 		userId: { type: mongoose.Schema.Types.ObjectId, ref: 'UserDetails', required: true }, // To associate the photo with a specific user
 		photo: { type: String, required: true }, // Path or URL of the photo
 		location: { type: String, required: true }, // Location where the photo was taken
+		folder_name: { type: String, required: true },
 		date: { type: String, required: true }, // Date the photo was taken
 		time: { type: String, required: true }, // Time the photo was taken
 		description: { type: String },
