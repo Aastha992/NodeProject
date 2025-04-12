e2c25cb5ae91424cf60fb676491b223153dfd037
Kartik Dangi <114815601+MrkartikDangi@users.noreply.github.com>
2025-04-12 13:12:40 +0530

    Update Logo.js

--- models/Logo.js
+++ models/Logo.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const LogoSchema = new mongoose.Schema({
-    companyName: { type: String, required: true },  // Name of the company
+    companyName: { type: String},  // Name of the company
+    folder_name: { type: String, required: true },  // Name of the company
     logoUrl: { type: String, required: true }  // URL where the logo is stored
 });
 
