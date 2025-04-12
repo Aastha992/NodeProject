6dc9e32cacb3a7f3a2fd8a0739bcf2cd8c9f5c86
Kartik Dangi <114815601+MrkartikDangi@users.noreply.github.com>
2025-04-12 17:50:03 +0530

    Update invoiceRoutes.js

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Invoice = require("../models/Invoice");
 const common = require("../utils/common.util");
+const ExcelJS = require('exceljs');
+const Project = require("../models/Projects")
+const fs = require('fs');
+const moment = require('moment')
+const authenticateJWT = require("../utils/authenticateJWT");
 
-router.get('/', async (req, res) => {
+router.get('/',authenticateJWT, async (req, res) => {
     const { invoiceId } = req.query;
 
     try {
@@ -27,7 +32,7 @@ router.get('/', async (req, res) => {
                 return common.error(req, res, { message: "Invoice not found" });
             }
             result.userDetails = result.userDetails.map((detail) => {
-                detail = { ...detail.userId, ...detail, userId: detail.userId._id , _id : undefined , __v : undefined };
+                detail = { ...detail.userId, ...detail, userId: detail.userId._id, _id: undefined, __v: undefined };
                 return detail;
             })
 
@@ -66,7 +71,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post("/create-invoice", async (req, res) => {
+router.post("/create-invoice",authenticateJWT, async (req, res) => {
     const {
         clientName,
         fromDate,
@@ -117,6 +122,191 @@ router.post("/create-invoice", async (req, res) => {
         return common.error(req, res, {
             message: "Error creating invoice.",
             error: error.message
+        });
+    }
+});
+
+router.post("/generateInvoiceExcel",authenticateJWT, async (req, res) => {
+
+    const missingFieldsResponse = common.checkMissingFields(
+        ["invoiceDate"],
+        req.body,
+        res
+    );
+    if (missingFieldsResponse) return;
+    try {
+        const invoiceDateMoment = moment.utc(req.body.invoiceDate);
+
+        const startOfDay = invoiceDateMoment.startOf('day').toDate();
+        const endOfDay = invoiceDateMoment.endOf('day').toDate();
+        const data = await Invoice.find({
+            createdAt: {
+                $gte: startOfDay,
+                $lt: endOfDay
+            }
+        });
+        if(data){
+            const workbook = new ExcelJS.Workbook();
+            const sheet = workbook.addWorksheet('KPS Monthly Invoicing Summary');
+    
+            const maxUserDetailsLength = Math.max(...data.map(item => item.userDetails.length));
+            const colLength = 5 + maxUserDetailsLength * 5;
+    
+            sheet.columns = Array(colLength + 2).fill({ width: 25 });
+    
+            const offset = 2;
+            const startCharCode = 65 + offset;
+            const startCol = String.fromCharCode(startCharCode);
+            const nextCol = String.fromCharCode(startCharCode + 1);
+            const endCol = String.fromCharCode(startCharCode + colLength - 1);
+    
+    
+            sheet.mergeCells(`${startCol}4:${endCol}4`);
+            sheet.getCell(`${startCol}4`).value = 'KPS Monthly Invoicing Summary';
+            sheet.getCell(`${startCol}4`).font = { size: 14, bold: true };
+            sheet.getCell(`${startCol}4`).alignment = { horizontal: 'left', vertical: 'left' };
+    
+    
+            const from = moment(data[0].fromDate).format('MMM-D-YYYY');
+            const to = moment(data[0].toDate).format('MMM-D-YYYY');
+    
+            sheet.mergeCells(`${startCol}5:${startCol}6`);
+            sheet.getCell(`${startCol}5`).value = 'Invoiced duration:';
+            sheet.getCell(`${startCol}5`).font = { size: 10, bold: true };
+            sheet.getCell(`${startCol}5`).alignment = { horizontal: 'left', vertical: 'middle' };
+    
+            sheet.mergeCells(`${nextCol}5:${endCol}6`);
+            sheet.getCell(`${nextCol}5`).value = `${from} to ${to}`;
+            sheet.getCell(`${nextCol}5`).alignment = { horizontal: 'left', vertical: 'middle' };
+    
+    
+            const uniqueUserNames = [
+                ...new Set(
+                    data.flatMap(item => item.userDetails.map(user => user.userName))
+                )
+            ];
+    
+            // Table Header
+            const tableHeader = [
+                '', '',
+                'Client',
+                'Invoice To',
+                'Project Name',
+                'Client PO/Reference Number',
+                'KPS Billing Description on Invoice',
+                ...uniqueUserNames,  // Dynamically add user names as headers
+                'totalBillableHours',
+                'rate',
+                'subTotal',
+                'total'
+            ];
+            const headerRow = sheet.addRow(tableHeader);
+    
+            headerRow.eachCell((cell, colNumber) => {
+    
+                const isNonEmpty = cell.value !== undefined && cell.value !== '';
+    
+                if (isNonEmpty) {
+                    cell.font = { bold: true };
+                    cell.fill = {
+                        type: 'pattern',
+                        pattern: 'solid',
+                        fgColor: { argb: 'FFD9D9D9' }
+                    };
+    
+                    cell.alignment = { horizontal: 'center', vertical: 'middle' };
+                    cell.border = {
+                        top: { style: 'thin' },
+                        bottom: { style: 'thin' },
+                        left: { style: 'thin' },
+                        right: { style: 'thin' }
+                    };
+                }
+    
+    
+            });
+    
+            sheet.getColumn(7).width = 50;
+            const totalHoursByUser = uniqueUserNames.map(() => 0);
+            for (let i = 0; i < data.length; i++) {
+                const invoice = data[i];
+                const project = await Project.findById(invoice.projectId);
+    
+                const dataRow = [
+                    '', '',
+                    invoice.clientName,
+                    invoice.invoiceTo,
+                    project ? project.projectName : 'Unknown Project',
+                    '',
+                    invoice.description,
+                ];
+    
+                uniqueUserNames.forEach((userName, index) => {
+                    const user = invoice.userDetails.find(u => u.userName === userName);
+                    const totalHours = user ? user.totalHours : 0;
+                    dataRow.push(totalHours);
+    
+                    totalHoursByUser[index] += totalHours;
+                });
+    
+                const summary = invoice.userDetails[0];
+                dataRow.push(
+                    invoice.totalBillableHours,
+                    summary.rate,
+                    invoice.subTotal,
+                    invoice.totalAmount
+                );
+    
+                const row = sheet.addRow(dataRow);
+    
+                const descriptionColIndex = 7;
+                const charCount = invoice.description ? invoice.description.length : 0;
+                const wrapCharsPerLine = 45;
+                const lineCount = Math.ceil(charCount / wrapCharsPerLine);
+                const estimatedHeight = Math.max(25, lineCount * 20);
+    
+                row.height = estimatedHeight;
+    
+                const descriptionCell = row.getCell(descriptionColIndex);
+                descriptionCell.alignment = {
+                    wrapText: true,
+                    vertical: 'top',
+                    horizontal: 'left'
+                };
+    
+                descriptionCell.border = {
+                    top: { style: 'thin', color: { argb: 'FFFFFFFF' } },
+                    left: { style: 'thin', color: { argb: 'FFFFFFFF' } },
+                    bottom: { style: 'thin', color: { argb: 'FFFFFFFF' } },
+                    right: { style: 'thin', color: { argb: 'FFFFFFFF' } }
+                };
+                row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+                    if (colNumber !== descriptionColIndex) {
+                        cell.alignment = { horizontal: 'right', vertical: 'middle' };
+                    }
+                });
+            }
+    
+            const totalRow = sheet.addRow([
+                '', 'Total Hours', '', '', '', '', '', ...totalHoursByUser, '', '', '', ''
+            ]);
+    
+            totalRow.font = { bold: true };
+    
+            const buffer = await workbook.xlsx.writeBuffer();
+            res.setHeader('Content-Disposition', 'attachment; filename=Invoice_Summary.xlsx');
+            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            res.send(buffer);
+
+        }else{
+            res.status(200).json({status:true , message: `Data is not available`})
+        }
+
+      
+    } catch (error) {
+        return common.error(req, res, {
+            message: "Error creating invoice.",
+            error: error
         });
     }
 });
